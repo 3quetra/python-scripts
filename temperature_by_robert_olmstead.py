f705ce71127f3e215e40fce22286561c1a157ecd
Julia <juliabukato@gmail.com>
2022-02-07 11:13:05 -0100

    Delete temperature_by_robert_olmstead.py

--- temperature_by_robert_olmstead.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('temperature_readings.csv', newline='') as temperature_readings:
-    temperature_reader = csv.DictReader(temperature_readings)
-
-print('''INSERT INTO Person (Taken, Temp)
-VALUES)
